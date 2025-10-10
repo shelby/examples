3b516cfdb243af59e1e1df61b89c341651635d2c
maayan <maayansavir@gmail.com>
2025-10-10 14:46:27 -0400

    small fix to use shelbynet

--- apps/cross-chain-accounts/components/WalletProvider.tsx
+++ apps/cross-chain-accounts/components/WalletProvider.tsx
@@ -6,15 +6,15 @@ import { Network } from "@aptos-labs/ts-sdk";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import type { PropsWithChildren } from "react";
 
-setupAutomaticSolanaWalletDerivation({ defaultNetwork: Network.DEVNET });
-setupAutomaticEthereumWalletDerivation({ defaultNetwork: Network.DEVNET });
+setupAutomaticSolanaWalletDerivation({ defaultNetwork: Network.SHELBYNET });
+setupAutomaticEthereumWalletDerivation({ defaultNetwork: Network.SHELBYNET });
 
 export const WalletProvider = ({ children }: PropsWithChildren) => {
   return (
     <AptosWalletAdapterProvider
       autoConnect={true}
       dappConfig={{
-        network: Network.DEVNET,
+        network: Network.SHELBYNET,
         aptosApiKeys: {
           devnet: process.env.NEXT_PUBLIC_SHELBY_API_KEY,
         },
